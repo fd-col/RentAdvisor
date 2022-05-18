9ded7d33f81736e8d3c312fb6d4aa53fed17fb80
AndreaCiv <s1092302@studenti.univpm.it>
2022-05-18 19:09:01 +0200

    Spezzettamento views pubbliche

--- laraProj_RentAdvisor/resources/views/layouts/_navpublic.blade.php
+++ laraProj_RentAdvisor/resources/views/layouts/_navpublic.blade.php
@@ -1,21 +1,30 @@
-<ul>
-    <li><a href="{{ route('catalog1') }}" title="Home">Catalogo</a></li>
-    <li><a href="{{ route('who') }}" title="Il nostro profilo aziendale">Chi siamo</a></li>
-    <li><a href="{{ route('where') }}" title="Dove trovarci">Dove Siamo</a></li>
-    <li><a href="mailto:info@acme.it" title="Mandaci un messaggio">Contattaci</a></li>
-    @can('isAdmin')
-        <li><a href="{{ route('admin') }}" class="highlight" title="Home Admin">Home Admin</a></li>
-    @endcan
-    @can('isUser')
-        <li><a href="{{ route('user') }}" class="highlight" title="Home User">Home User</a></li>
-    @endcan
-    @auth
-        <li><a href="" title="Esci dal sito" class="highlight" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">Logout</a></li>
-        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-            {{ csrf_field() }}
-        </form>
-    @endauth    
-    @guest
-        <li><a href="{{ route('login') }}" class="highlight" title="Accedi all'area riservata del sito">Accedi</a></li>  
-    @endguest
-</ul>
+<!-- Start menu section -->
+<section id="aa-menu-area">
+    <nav class="navbar navbar-default main-navbar" role="navigation">
+        <div class="container">
+            <div class="navbar-header">
+                <!-- FOR MOBILE VIEW COLLAPSED BUTTON -->
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <!-- LOGO -->
+                <!-- Text based logo -->
+                <a class="navbar-brand aa-logo" href="{{ route('home') }}"> <span>RentAdvisor</span> </a>
+                <!-- Image based logo -->
+                <!-- <a class="navbar-brand aa-logo-img" href="index.blade.php"><img src="img/logo.png" alt="logo"></a> -->
+            </div>
+            <div id="navbar" class="navbar-collapse collapse">
+                <ul id="top-menu" class="nav navbar-nav navbar-right aa-main-nav">
+                    <li class="active"><a href="{{ route('home') }}">HOME</a></li>
+                    <li><a href="{{ route('catalog') }}">CATALOGO</a></li>
+                    <li><a href="{{ route('contact') }}">CONTATTI</a></li>
+                    <li><a href="{{ route('faq') }}">FAQ</a></li>
+                </ul>
+            </div><!--/.nav-collapse -->
+        </div>
+    </nav>
+</section>
+<!-- End menu section -->
