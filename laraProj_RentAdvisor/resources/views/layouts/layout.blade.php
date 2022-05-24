8f959ed527549b0fe1f6d7ca0ec00f38a0476ada
AndreaCiv <s1092302@studenti.univpm.it>
2022-05-24 12:31:00 +0200

    include jquery

--- laraProj_RentAdvisor/resources/views/layouts/layout.blade.php
+++ laraProj_RentAdvisor/resources/views/layouts/layout.blade.php
@@ -194,6 +194,7 @@
 <!-- jQuery library -->
 <!--<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script> -->
 <script src="{{ asset('js/jquery.min.js') }}"></script>
+<script src="{{ asset('js/jquery.js') }}"></script>
 <!-- Include all compiled plugins (below), or include individual files as needed -->
 <script src="{{ asset('js/bootstrap_layout.js') }}"></script>
 <!-- slick slider -->
