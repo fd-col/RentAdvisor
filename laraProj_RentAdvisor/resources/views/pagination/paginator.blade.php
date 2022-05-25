7c784df3defe514224e7e91e8a539fe1208ada2d
AndreaCiv <s1092302@studenti.univpm.it>
2022-05-25 09:40:51 +0200

    Modifica slider pagine

--- laraProj_RentAdvisor/resources/views/pagination/paginator.blade.php
+++ laraProj_RentAdvisor/resources/views/pagination/paginator.blade.php
@@ -1,29 +1,31 @@
 <!-- Start properties content bottom -->
 @if ($paginator->lastPage() != 1)
     <div class="aa-properties-content-bottom">
-        <nav>
-            <ul class="pagination">
-                <li>
-                    <!-- Link alla pagina precedente -->
-                    @if ($paginator->currentPage() != 1)
-                        <a href="{{ $paginator->previousPageUrl() }}" aria-label="Previous"><span aria-hidden="true">&laquo;</span></a>
-                    @else
-                        <span aria-hidden="true">&laquo;</span>
-                    @endif
-                </li>
-                @for($i = 1; $i <= $paginator->lastPage(); $i++)
-                    <li class="{{ $paginator->currentPage()==$i ? 'active' : '' }}"><a href="{{ $paginator->url($i) }}">{{ $i }}</a></li>
-                @endfor
-                <li>
-                    <!-- Link alla pagina successiva -->
-                    @if ($paginator->hasMorePages())
-                        <a href="{{ $paginator->nextPageUrl() }}" aria-label="Next"><span aria-hidden="true">&raquo;</span></a>
-                    @else
-                        <span aria-hidden="true">&raquo;</span>
-                    @endif
-                </li>
-            </ul>
-        </nav>
+        <div class="col-md-4">
+            <nav>
+                <ul class="pagination">
+                    <li>
+                        <!-- Link alla pagina precedente -->
+                        @if ($paginator->currentPage() != 1)
+                            <a href="{{ $paginator->previousPageUrl() }}" aria-label="Previous"><span aria-hidden="true">&laquo;</span></a>
+                        @else
+                            <span aria-hidden="true">&laquo;</span>
+                        @endif
+                    </li>
+                    @for($i = ($paginator->currentPage()-2 >= 1 ? $paginator->currentPage()-2 : 1); $i <= $paginator->lastPage() and $i <= $paginator->currentPage()+2; $i++)
+                        <li class="{{ $paginator->currentPage()==$i ? 'active' : '' }}"><a href="{{ $paginator->url($i) }}">{{ $i }}</a></li>
+                    @endfor
+                    <li>
+                        <!-- Link alla pagina successiva -->
+                        @if ($paginator->hasMorePages())
+                            <a href="{{ $paginator->nextPageUrl() }}" aria-label="Next"><span aria-hidden="true">&raquo;</span></a>
+                        @else
+                            <span aria-hidden="true">&raquo;</span>
+                        @endif
+                    </li>
+                </ul>
+            </nav>
+        </div>
     </div>
 @endif
 <!-- / End properties content bottom -->
